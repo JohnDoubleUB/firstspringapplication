31620947cf5ea2a80db97bfb519efcaf851ec739
Sehher <sehher.tariq@academytrainee.com>
2019-09-09 12:04:48 +0100

    Created Repository and Models folder Created Note (With sets and gets) and NoteRepository classes ready for the next step

---
+++ src/main/java/com/qa/repository/NoteRepository.java
@@ -0,0 +1,8 @@
+package com.qa.repository;
+
+import com.qa.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
