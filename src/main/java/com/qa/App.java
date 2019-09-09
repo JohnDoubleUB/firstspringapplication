96f4381738c9e91df96c079b11b68ce9e1073540
Sehher <sehher.tariq@academytrainee.com>
2019-09-09 11:14:01 +0100

    Added HomeController tag on level below our spring application

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -3,6 +3,8 @@ package com.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//In combination with the SpringApplication.run, it will find all the files under this package directory!
 @SpringBootApplication
 public class App 
 {
