71beb17de9764109d6f06b99d0e3dce81a865722
Sehher <sehher.tariq@academytrainee.com>
2019-09-09 11:25:22 +0100

    Added @RestController and a request @RequestMapping and a function home(); By Default this will appear on 8080!

--- src/main/java/com/qa/controllers/HomeController.java
+++ src/main/java/com/qa/controllers/HomeController.java
@@ -1,5 +1,16 @@
 package com.qa.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//Rest contoller makes this available to spring
+
+@RestController
 public class HomeController {
+
+    //When someone hits a specific address this happens
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
 
 }
