e4690f486eadf11016e301de15fd6e058ca08001
Sehher <sehher.tariq@academytrainee.com>
2019-09-09 11:32:10 +0100

    Added resources directory, marked as resources. Changed application port from 8080 to 9000

--- src/main/java/com/qa/controllers/HomeController.java
+++ src/main/java/com/qa/controllers/HomeController.java
@@ -3,7 +3,8 @@ package com.qa.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //Rest contoller makes this available to spring
-
+//Rest controller is the new controller
+//Controller is the old one and would require a data type conversion type in the function itself
 @RestController
 public class HomeController {
 
